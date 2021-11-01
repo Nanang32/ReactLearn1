531c2154ec51c1ffb62535b1e51653a85d5b2c81
Luqman Fanani <luqman.fanani@gmail.com>
2021-11-01 14:57:46 +0800

    stateless componen

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
-import React, { Component } from "react";
+import React, {Component, useState} from "react";
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      firstName: '',
-      lastName: '',
-      name: ''
-    }
-  }
+function App(){
+  const [firstName,setFirstname] = useState('')
+  const [lastName,setLastname] = useState('')
+  const [name,setName] = useState('')
 
-  changeHandler = (e)=>{
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  submitHandler = (e)=>{
+  const submitHandler = (e) =>{
     e.preventDefault()
-    this.setState({
-      name: `${this.state.firstName} ${this.state.lastName} `
-    })
-
-    this.state.firstName = ''
-    this.state.lastName = ''
+    setName(`${firstName} ${lastName}` )
+    setFirstname('')
+    setLastname('')
   }
-  render(){
-    const {name} = this.state
-    return(
+  return (
       <div className="p-5">
         <div className="container">
-          <div className="col-md-4">
-            <div className="card">
-              <div className="card-header">Learn React</div>
-              <div className="card-body">
-                <form onSubmit={this.submitHandler}>
-                  <div className="mb-4">
-                    <label htmlFor="firstName" className="form-label">FirstName</label>
-                    <input type="text" value={this.state.firstName} onChange={this.changeHandler} name="firstName" id="firstName" className="form-control"></input>
-                  </div>
-                  <div className="mb-4">
-                    <label htmlFor="lastName" className="form-label">LastName</label>
-                    <input type="text" value={this.state.lastName} onChange={this.changeHandler} name="lastName" id="lastName" className="form-control"></input>
-                  </div>
-                  <button type="submit" className="btn btn-primary btn-block">Show</button>
-                </form>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card">
+                <div className="card-header">Learn React Function</div>
+                <div className="card-body">
+                  <form onSubmit={submitHandler}>
+                    <div className="mb-4">
+                      <label htmlFor="firstName" className="form-label">FirstName</label>
+                      <input type="text" value={firstName} onChange={(e)=> setFirstname(e.target.value)}  name="firstName" id="firstName" className="form-control"></input>
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="lastName" className="form-label">LastName</label>
+                      <input type="text" value={lastName} onChange={(e)=> setLastname(e.target.value)}  name="lastName" id="lastName" className="form-control"></input>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Show</button>
+                  </form>
+                </div>
+                <div className="card-footer">My name is {name ? name : '...'}</div>
               </div>
-              <div className="card-footer">My name is {name ? name : '...'}</div>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+  )
 }
 
 export default App;
