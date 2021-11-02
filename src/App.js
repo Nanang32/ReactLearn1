07ec8854b2d67a176d02e39ec2ccfcc4a39e3ae0
Luqman Fanani <luqman.fanani@gmail.com>
2021-11-02 13:38:16 +0800

    fetch data with Useeffect & loading

--- src/App.js
+++ src/App.js
@@ -3,58 +3,55 @@ import axios from 'axios';
 
 
 function App(props) {
+    const [identifier, setIdentifier] = useState(1);
     const [user, setUser] = useState([]);
-    const getUser = async ()=>{
+    const [loading, setLoading] = useState(false)
+    const getUser = async () => {
+        setLoading(true)
         try {
-            let response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+            let response = await axios.get(`https://jsonplaceholder.typicode.com/users/${identifier}`)
             setUser(response.data)
-        } catch (e){
+            setLoading(false)
+        } catch (e) {
+            setLoading(true)
             console.log(e.message);
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         getUser();
-    },[])
+    }, [identifier])
     return (
         <div className="py-4">
             <div className="container">
-               <div className="row justify-content-center">
-                   <div className="col-md-8">
-                       <table className="table">
-                           <thead>
-                                <tr>
-                                   <th>Name</th>
-                                   <th>Username</th>
-                                   <th>Email</th>
-                                   <th>Website</th>
-                                   <th>phone</th>
-                                </tr>
-                           </thead>
-                           <tbody>
-                               <tr >
-                                   <td>{user.name}</td>
-                                   <td>{user.username}</td>
-                                   <td>{user.email}</td>
-                                   <td>{user.website}</td>
-                                   <td>{user.phone}</td>
-                               </tr>
-                               {/*{*/}
-                               {/*    users.map((user,index)=>{*/}
-                               {/*        return(*/}
-                               {/*            <tr key={index}>*/}
-                               {/*                <td>{user.name}</td>*/}
-                               {/*                <td>{user.username}</td>*/}
-                               {/*                <td>{user.email}</td>*/}
-                               {/*                <td>{user.website}</td>*/}
-                               {/*                <td>{user.phone}</td>*/}
-                               {/*            </tr>*/}
-                               {/*        )*/}
-                               {/*    })*/}
-                               {/*}*/}
-                           </tbody>
-                       </table>
-                   </div>
-               </div>
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <input type="text" name="identifier" value={identifier}
+                               onChange={(e) => setIdentifier(e.target.value)} className="form-control"/>
+                        {
+                                loading ? <div>Loading ...</div> :
+                                <table className="table">
+                                    <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Website</th>
+                                        <th>phone</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>{user.name}</td>
+                                        <td>{user.username}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.website}</td>
+                                        <td>{user.phone}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )
